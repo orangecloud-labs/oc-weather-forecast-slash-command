48175efd0d5c000c3743fa7c3fdeeeeaec7bea70
m4r35 <marko.marjanovic.dev@gmail.com>
2017-02-04 07:53:43 +0100

    delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {   
-	res.writeHead(200, {'Content-Type': 'text/plain'});   
-	res.end('Hello World\n'); 
-}).listen(3000, '46.101.227.175'); 
-console.log('Server running at http://46.101.227.175:3000/');
