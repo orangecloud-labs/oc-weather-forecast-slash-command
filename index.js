c237b518759c0302b4fea2d3c8fbd02d8599c573
m4r35 <marko.marjanovic.dev@gmail.com>
2017-02-04 07:26:37 +0100

    add day forecast feature

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 var url = require('url');
 var request = require('request');
+var config = require('./config');
 
-var format = ".json";
-var apikey =  'ddd6c2810045c862'; //WU API key
+var apikey =  config.wu;
 
 var bodyParser = require('body-parser');
 
@@ -18,27 +18,72 @@ app.get('/', function(req, res){   res.send('Running!!'); });
 app.post('/weather', function(req, res){   
 	
 	var query = req.body.text;
+	var splitedQuery = query.split(/[\s]\-/);
+	var place = splitedQuery[0];
+	var flag = splitedQuery[1];
 
-	var parsed_url = url.format({     
-		pathname: 'http://api.wunderground.com/api/' + apikey + '/conditions/q/' + req.body.text + format,   
-	});
-	console.log(parsed_url);
-	request(parsed_url, function (error, response, body) {     
-		var d = JSON.parse(body);
-		//console.log(d);
-		if(d.hasOwnProperty('current_observation') === false){ 
-			res.send('No cities match your search query'); 
-			return;
+	if(typeof flag != 'undefined'){
+		switch(flag){
+			case 'd':
+				var parsed_url = url.format({     
+					pathname: 'http://api.wunderground.com/api/' + apikey + '/forecast/q/' + place + '.json',   
+				});
+				
+				request(parsed_url, function (error, response, body) {     
+					var d = JSON.parse(body);
+					if(d.hasOwnProperty('forecast') === false){ 
+						res.send('No cities match your search query'); 
+						return;
+					}
+					if (!error && response.statusCode == 200) {       
+						var data = JSON.parse(body);       
+						var day = data.forecast.simpleforecast.forecastday[0];
+						var icon_url = day.icon_url;       
+						var location = place;
+						var body = {         
+							response_type: "in_channel",
+			        	 			"attachments": [{            
+								"author_name": "Orange Cloud",
+								"color":"#00BFFF",
+								"title": "Day forecast, " + day.date.pretty,
+				 				"text": "" + location + ", *low " + day.low.celsius + "°C " + "" + ", high " + day.high.celsius +"°C*\n"                   
+								+ data.forecast.txt_forecast.forecastday[0].fcttext_metric,             
+								"image_url": icon_url + "\n",           
+								"footer": "<https://github.com/m4r35>",
+								"mrkdwn_in": [
+						         		"text"       
+								]
+					  			}]       
+						};       
+						res.send(body);
+						return;     
+					}
+					res.send('Error ocurred, please try again later!');   
+					return;
+				}); 
+			break;
+			default:
+				return;	
 		}
-		if (!error && response.statusCode == 200) {       
-			var data = JSON.parse(body);       
-			var temperature = data.current_observation.temp_c;       
-			var weatherCondition = data.current_observation.weather       
-			var icon_url = data.current_observation.icon_url       
-			var location = data.current_observation.display_location.full        
-			var body = {         
-				response_type: "in_channel",
-			        	 "attachments": [{            
+	}else{
+		var parsed_url = url.format({     
+			pathname: 'http://api.wunderground.com/api/' + apikey + '/conditions/q/' + place + ".json",   
+		});
+		request(parsed_url, function (error, response, body) {     
+			var d = JSON.parse(body);
+			if(d.hasOwnProperty('current_observation') === false){ 
+				res.send('No cities match your search query'); 
+				return;
+			}
+			if (!error && response.statusCode == 200) {       
+				var data = JSON.parse(body);       
+				var temperature = data.current_observation.temp_c;       
+				var weatherCondition = data.current_observation.weather       
+				var icon_url = data.current_observation.icon_url       
+				var location = data.current_observation.display_location.full        
+				var body = {         
+					response_type: "in_channel",
+			        	 	"attachments": [{            
 						"author_name": "Orange Cloud",
 						"color":"#00BFFF",
 						"title": "" + data.current_observation.observation_time,
@@ -49,14 +94,15 @@ app.post('/weather', function(req, res){
 						"mrkdwn_in": [
 						         "text"       
 						]
-					  }]       
-			};       
-			res.send(body);
-			return;     
-		}
-		res.send('Error ocurred, please try again later!');   
-		return;
-	}); 
+					  	}]       
+				};       
+				res.send(body);
+				return;     
+			}
+			res.send('Error ocurred, please try again later!');   
+			return;
+		}); 
+	}
 });  
 
 app.listen(app.get('port'), function() {   
